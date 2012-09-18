05a6f19db9e83f6dba17019385f2cf323704f692
unknown <pc@pc1.(none)>
2012-09-18 00:15:28 -0300

    Linea de Bienvenida

--- index.php
+++ index.php
@@ -243,6 +243,7 @@ $app_name = idx($app_info, 'name', '');
       <?php } else { ?>
       <div>
         <h1>Welcome</h1>
+	<h2>Esto es una aplicación hecha en <strong>Heroku</strong> donde más</h2>
         <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
       </div>
       <?php } ?>
